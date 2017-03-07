c62e1d7a5bc5d9614fccf0fc9dabf72a78a20df3
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-07 14:00:36 -0500

    Add vignettes (converted from README)

---
+++ inst/doc/usage.R
@@ -0,0 +1,38 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  devtools::install_github("sherrillmix/taxonomizr")
+
+## ------------------------------------------------------------------------
+library(taxonomizr)
+
+## ----eval=FALSE----------------------------------------------------------
+#  getNamesAndNodes()
+
+## ----eval=FALSE----------------------------------------------------------
+#  #this is a big download
+#  getAccession2taxid()
+
+## ----eval=FALSE----------------------------------------------------------
+#  #this is a big download
+#  getAccession2taxid(types='prot')
+
+## ----eval=FALSE----------------------------------------------------------
+#  read.accession2taxid(list.files('.','accession2taxid.gz$'),'accessionTaxa.sql')
+
+## ----eval=FALSE----------------------------------------------------------
+#  taxaNodes<-read.nodes('nodes.dmp')
+#  taxaNames<-read.names('names.dmp')
+
+## ----eval=FALSE----------------------------------------------------------
+#  taxaId<-accessionToTaxa(c("LN847353.1","AL079352.3"),"accessionTaxa.sql")
+#  print(taxaId)
+
+## ----eval=FALSE----------------------------------------------------------
+#  getTaxonomy(taxaId,taxaNodes,taxaNames)
+
+## ----eval=FALSE----------------------------------------------------------
+#  taxaId<-getId(c('Homo sapiens','Bos taurus','Homo'),taxaNames)
+#  print(taxaId)
+
+## ----eval=FALSE----------------------------------------------------------
+#  getTaxonomy(taxaId,taxaNodes,taxaNames)
+
--- makefile
+++ makefile
@@ -20,16 +20,18 @@ man: R/*.R
 	R -e 'devtools::document()'
 	touch man
 
+vignettes/usage.Rmd: usage.template README.Rmd
+	cp usage.template vignettes/usage.Rmd
+	sed -n '/\#\# Introduction/,$$p' README.Rmd >> vignettes/usage.Rmd
 
-#inst/doc: vignettes/*.Rnw
-	#R -e 'devtools::build_vignettes()'
-	#touch inst/doc
+inst/doc: vignettes/usage.Rmd
+	R -e 'devtools::build_vignettes()'
+	touch inst/doc
 
 README.md: README.Rmd R/*.R
 	make localInstall
 	R -e 'knitr::opts_chunk$$set(fig.path="README_files/");knitr::knit("README.Rmd")'
 	
-#inst/doc 
-$(PACKAGEFILE): man R/*.R DESCRIPTION src/*.c tests/testthat/*.R
+$(PACKAGEFILE): man R/*.R DESCRIPTION src/*.c tests/testthat/*.R inst/doc
 	sed -i "s/^Date:.*$$/Date: `date +%Y-%m-%d`/" DESCRIPTION
 	R -e 'devtools::check();devtools::build()'
