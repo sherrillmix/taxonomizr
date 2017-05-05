9ff6d95e76a1b414d26527c337edc3a534b0b377
sherrillmix <sherrillmix@users.noreply.github.com>
2017-05-05 16:07:17 -0400

    Escape columns

--- R/taxa.R
+++ R/taxa.R
@@ -557,7 +557,7 @@ condenseTaxa2<-function(taxaTable,groupings=rep(1,nrow(taxaTable))){
   if(is.null(colnames(taxaTable)))colnames(taxaTable)<-sprintf("V%d",1:ncol(taxaTable))
   RSQLite::dbWriteTable(tmpDb,'tmp',cbind(as.data.frame(taxaTable,stringsAsFactors=FALSE),'id'=groupings),overwrite=TRUE)
   RSQLite::dbGetQuery(tmpDb,"CREATE INDEX index_id ON tmp (id)")
-  colSelects<-sprintf('GROUP_CONCAT(DISTINCT(%s)) AS %s',colnames(taxaTable),colnames(taxaTable))
+  colSelects<-sprintf('GROUP_CONCAT(DISTINCT(`%s`)) AS `%s`',colnames(taxaTable),colnames(taxaTable))
   query<-sprintf("SELECT id, %s FROM tmp GROUP BY id",paste(colSelects,collapse=', '))
   out<-RSQLite::dbGetQuery(tmpDb,query)
   out<-t(apply(out,1,function(xx){
